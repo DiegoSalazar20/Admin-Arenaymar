71f92e17bdd7e389de4c771ac3a2fa836166dd91
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-05-04 14:47:33 -0600

    Validacion de expiracion del token

--- src/app/guards/auth.guards.ts
+++ src/app/guards/auth.guards.ts
@@ -1,6 +1,11 @@
 import { inject, Inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
+import { jwtDecode } from 'jwt-decode';
+
+interface TokenPayload {
+  exp: number;
+}
 
 export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
@@ -8,9 +13,21 @@ export const authGuard: CanActivateFn = () => {
   
   if (isPlatformBrowser(platformId)) {
     const token = localStorage.getItem('token');
+
     if (token) {
-        return true;
+      try {
+        const decodificado: TokenPayload = jwtDecode(token);
+        const horaActual = Math.floor(Date.now() / 1000);
+
+        if (decodificado.exp && decodificado.exp > horaActual) {
+          return true;
+        }
+      } catch (err) {
+        console.error('Error con el token:', err);
       }
+
+      localStorage.removeItem('token');
+    }
   }
     router.navigate(['/']);
     return false;
