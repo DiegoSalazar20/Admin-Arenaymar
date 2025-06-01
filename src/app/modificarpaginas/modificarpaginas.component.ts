d09a466b918ced5935caebcf0d0cfd883d4c394d
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-06-01 11:25:29 -0600

    Pagina para modificar las diferentes paginas del modulo de clientes

--- src/app/modificarpaginas/modificarpaginas.component.ts
+++ src/app/modificarpaginas/modificarpaginas.component.ts
@@ -1,11 +1,64 @@
 import { Component } from '@angular/core';
 import { MenuadminComponent } from '../menuadmin/menuadmin.component';
+import { OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-modificarpaginas',
-  imports: [MenuadminComponent],
+  imports: [MenuadminComponent, CommonModule,
+    HttpClientModule, FormsModule],
   templateUrl: './modificarpaginas.component.html',
   styleUrl: './modificarpaginas.component.scss'
 })
 export class ModificarpaginasComponent {
+
+  paginaSeleccionada: string = '';
+  modificarSobrenosotros: boolean = false;
+  cerrandoModalSobreNosotros: boolean = false;
+
+  sobreNosotros: any = {
+    id: '',
+    texto: 0
+  };
+  cargandoAccion: boolean = false;
+
+  private urlSobreNosotros='https://arenaymar-frdyg5caarhsd2g5.eastus-01.azurewebsites.net/api/SobreNosotros';
+
+  constructor(private http: HttpClient) { }
+
+  obtenerSobreNosotros() {
+  this.http.get<any>(this.urlSobreNosotros)
+    .subscribe({
+      next: (data) => {
+        this.sobreNosotros.id = data.id;
+        this.sobreNosotros.texto = data.texto;
+      },
+      error: (err) => {
+        console.error('Error al obtener la información de Sobre Nosotros', err);
+      }
+    });
+}
+
+  abrirModal() {
+  if (this.paginaSeleccionada === 'sobre-nosotros') {
+    this.obtenerSobreNosotros();
+    this.modificarSobrenosotros=true;
+  } else {
+    console.log('Página no implementada aún');
+  }
+}
+
+onAnimationEnd(tipo: 'modalSobreNosotros' | 'Notificacion' | 'Confirmacion') {
+  if (tipo === 'modalSobreNosotros' && this.cerrandoModalSobreNosotros) {
+    this.cerrandoModalSobreNosotros = false;
+    this.modificarSobrenosotros = false;
+  }
+}
+
+cerrarModal() {
+  this.cerrandoModalSobreNosotros = true;
+}
+
 
 }
